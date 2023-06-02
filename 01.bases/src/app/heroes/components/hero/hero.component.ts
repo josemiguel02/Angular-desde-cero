7db5ae9696d3a3f929dfe815e128b6b974cb5f3f
josemiguel02 <josemidev24@gmail.com>
2023-06-02 10:23:51 -0500

    end section 4

--- 01.bases/src/app/heroes/components/hero/hero.component.ts
+++ 01.bases/src/app/heroes/components/hero/hero.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.css'],
 })
 export class HeroComponent {
   public name: string = 'Ironman';
--- 01.bases/src/app/heroes/components/list/list.component.ts
+++ 01.bases/src/app/heroes/components/list/list.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
   public heroes: string[] = [
