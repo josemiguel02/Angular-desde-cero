487e3e342ff7d1a8a79a5461de423e2a09b68413
josemiguel02 <josemidev24@gmail.com>
2023-07-05 11:47:21 -0500

    ⭐️ End section 11

--- 04.pipes-app/src/app/products/pages/numbers-page/numbers-page.component.ts
+++ 04.pipes-app/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NumbersPageComponent {
 
+  public totalSells: number = 2567789.5567;
+  public percent: number = 0.4856;
 }
--- 04.pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 04.pipes-app/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,8 +1,68 @@
 import { Component } from '@angular/core';
+import { interval, tap } from 'rxjs';
+
+type Gender = 'male' | 'female';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
   styles: [],
 })
-export class UncommonPageComponent {}
+export class UncommonPageComponent {
+
+  // i18n Select
+  public name: string = 'Jose';
+  public gender: Gender = 'male';
+  public invitationMap: Record<Gender, string> = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
+
+
+  // i18n Plural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.'
+  };
+
+  deleteClient() {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'José Miguel',
+    age: 23,
+    address: 'Gye/Ecu',
+  }
+
+  // Async Pipe
+  public myObservableTimer = interval(2000)
+    .pipe(
+      tap(value => console.log('tap:', value))
+    );
+
+  public myPromise = new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.')
+
+      console.log('Promise execute...')
+    }, 3500);
+  });
+}
