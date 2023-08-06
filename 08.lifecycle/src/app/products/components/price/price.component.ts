a286615616b9f3a6c657d0ac1501ec9856c91463
josemiguel02 <josemidev24@gmail.com>
2023-08-06 12:40:55 -0500

    ⚙️ Lifecycle App Setup

---
+++ 08.lifecycle/src/app/products/components/price/price.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  templateUrl: './price.component.html',
+  styles: [],
+})
+export class PriceComponent {}
