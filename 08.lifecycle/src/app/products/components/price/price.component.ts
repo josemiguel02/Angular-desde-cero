ebc56978231b3a27b5bded84977c684bd9c6be46
josemiguel02 <josemidev24@gmail.com>
2023-08-06 23:51:40 -0500

    feat: :sparkles: add lifecycle hooks in product.component

--- 08.lifecycle/src/app/products/components/price/price.component.ts
+++ 08.lifecycle/src/app/products/components/price/price.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styles: [],
 })
-export class PriceComponent {}
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  ngOnInit(): void {
+    console.log('Component Child: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Component Child: ngOnChanges');
+    console.log({ changes });
+  }
+
+  ngOnDestroy(): void {
+    console.log('Component Child: ngOnDestroy');
+  }
+}
--- 08.lifecycle/src/app/products/pages/product/product.component.ts
+++ 08.lifecycle/src/app/products/pages/product/product.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-product-page',
   templateUrl: './product.component.html',
   styles: [],
 })
-export class ProductComponent {}
+export class ProductComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+
+  public isProductVisible: boolean = false;
+  public currentPrice: number = 10;
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  increasePrice() {
+    this.currentPrice++;
+  }
+}
