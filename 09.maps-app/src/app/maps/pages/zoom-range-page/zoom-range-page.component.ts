a257990d672c32be58e6210704f5230f4536271c
josemiguel02 <josemidev24@gmail.com>
2023-08-20 01:32:05 -0500

    feat(maps): ✨ Add currentLngLat in ZoomRange Page

--- 09.maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 09.maps-app/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,17 +1,18 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+import { Map, LngLat } from 'mapbox-gl';
 
 @Component({
   templateUrl: './zoom-range-page.component.html',
   styleUrls: ['./zoom-range-page.component.css'],
 })
-export class ZoomRangePageComponent implements AfterViewInit {
+export class ZoomRangePageComponent implements AfterViewInit, OnDestroy {
 
   @ViewChild('map')
   private mapRef?: ElementRef<HTMLDivElement>;
   private map?: Map;
 
   public zoom: number = 10;
+  public currentLngLat: LngLat = new LngLat(-79.89057384725469, -2.202029452027375);
 
   ngAfterViewInit(): void {
     if (!this.mapRef) throw 'El elemento HTML no fue encontrado';
@@ -19,11 +20,15 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map = new Map({
       container: this.mapRef.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
+      center: this.currentLngLat, // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
     });
 
     this.mapListener();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
   }
 
   private mapListener() {
@@ -36,6 +41,10 @@ export class ZoomRangePageComponent implements AfterViewInit {
     this.map.on('zoomend', e => {
       if (this.map!.getZoom() <= 18) return;
       this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
     });
   }
 
